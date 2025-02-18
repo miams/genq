d1c117308410e7446fcd74ae93385834b6049575
Michael Iams <michael@iams.name>
2025-02-17 23:04:17 -0500

    fix install scripts

--- installers/rmgc-full-install-Win11.ps1
+++ installers/rmgc-full-install-Win11.ps1
@@ -1,9 +1,29 @@
 # This script must be run in Powershell as Administrative user.
-# Run these three commands first to enable script execution, download this file, and run this file.
+# Run these three commands prior to running script to enable script execution, download this file, and run this file.
 
-# Set-ExecutionPolicy Unrestricted
-# powershell -Command Invoke-WebRequest -Uri "https://raw.githubusercontent.com/miams/rmgc/refs/heads/main/installers/rmgc-full-install-Win11.ps1" -OutFile "rmgc-full-install-Win11.ps1"
-# rmgc-full-install-Win11.ps1
+#    Set-ExecutionPolicy Unrestricted
+#    powershell -Command Invoke-WebRequest -Uri "https://raw.githubusercontent.com/miams/rmgc/refs/heads/main/installers/rmgc-full-install-Win11.ps1" -OutFile "rmgc-full-install-Win11.ps1"
+#    rmgc-full-install-Win11.ps1
+
+# Process Parameters
+param (
+    [string]$OptionalString,
+    [int]$OptionalNumber,
+    [switch]$OptionalFlag
+)
+
+# Script logic using the parameters
+if ($OptionalString) {
+    Write-Host "Optional String: $OptionalString"
+}
+
+if ($OptionalNumber) {
+    Write-Host "Optional Number: $OptionalNumber"
+}
+
+if ($OptionalFlag) {
+    Write-Host "Optional Flag is present."
+}
 
 
 # Permit Powershell to software.
@@ -43,10 +63,45 @@ if ($answer -eq "y" -or $answer -eq "Y") {
 
 Write-Host `n
 Write-Host "RMGC is successfully installed." -ForegroundColor Green
+
+
+
+# There are three ways to enter Nushell: 
+#   1.  From within Powershell, type: nu <enter>
+#   2.  From Terminal, Open the dropdown menu by clicking on the 'down carat' You will see Nushell as an option
+
 Write-Host "Additionally, the next time you open the Terminal program, click on the 'down carat' for the drop-down menu.  You will see Nushell is a new option available. If you choose, you can go into Settings from the drop-down menu, and make Nushell your default shell in Terminal." 
 Write-Host `n
 Write-Host "Type 'nu' at the prommpt. Then begin having fun with RMGC by typing: rmgc [tab key]" 
 
-# This is erroring
+
+# $answer = Read-Host "Would you like (y/n) "
+
+# Create shortcut and save to programs section of menu bar
+$SourceExe = "$env:USERPROFILE\AppData\Local\Programs\nu\bin\nu.exe"
+$ArgumentsToSourceExe = ""
+$DestinationPath = "$env:AppData\Microsoft\Windows\Start Menu\Programs\rmgc.lnk"
+# param ( [string]$SourceExe, [string]$ArgumentsToSourceExe, [string]$DestinationPath )
+$WshShell = New-Object -COMObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut($DestinationPath)
+$Shortcut.TargetPath = $SourceExe
+$Shortcut.Arguments = $ArgumentsToSourceExe
+$Shortcut.Save()
+
+
+# https://files.rootsmagic.com/RM10/RootsMagic10Setup.exe
+# https://files.rootsmagic.com/RM10/RootsMagic10SetupX64.exe
+
+# https://files.rootsmagic.com/RM9/RootsMagic9Setup.exe
+# https://files.rootsmagic.com/RM9/RootsMagic9SetupX64.exe
+
+# https://files.rootsmagic.com/RM8/RootsMagic8Setup.exe
+
+# https://files.rootsmagic.com/RM10/RootsMagic10.dmg
+# https://files.rootsmagic.com/RM9/RootsMagic9.dmg
+# https://files.rootsmagic.com/RM8/RootsMagic8.dmg
+
+
+# This is erroring.  TODO: find way to reset permissions to normal
 # Set-ExecutionPolicy Restricted
 
--- installers/rmgc-install-MacOS.nu
+++ installers/rmgc-install-MacOS.nu
@@ -19,7 +19,7 @@ mkdir $"($env.AppData)/nushell/vendor/autoload"
 
 # Load a sample RootsMagic database.
 print "Installing sample RootsMagic database." 
-powershell -Command $"Invoke-WebRequest -Uri \"https://raw.githubusercontent.com/miams/rmgc/8d3fab75dab83b157e510b352065f152126cf6fa/demo/pres2020.rmtree\" -OutFile "$env:USERPROFILE\\Apps\\rmgc\\data\\pres2020.rmtree\""
+powershell -Command $"Invoke-WebRequest -Uri \"https://raw.githubusercontent.com/miams/rmgc/8d3fab75dab83b157e510b352065f152126cf6fa/demo/pres2020.rmtree\" -OutFile "$nu.home-path\\Apps\\rmgc\\data\\pres2020.rmtree\""
 
 # With fresh install we are defaulting to a demo mode. 
 # Create startup config file with values for demo 
@@ -27,20 +27,20 @@ print "Configuring to use demo mode with fresh install."
 
 '# Configuration for RMGC' | save $"($env.AppData)/nushell/vendor/autoload/rmgc-config.nu"
 
-echo $"\n $env.rmgc-mode = 'demo' \n $env.rmdb = '($env.USERPROFILE)/Apps/rmgc/data/pres2020.rmtree'" | tee { save --append $"($env.AppData)/nushell/vendor/autoload/rmgc-config.nu" }
+echo $"\n $env.rmgc-mode = 'demo' \n $env.rmdb = '($nu.home-path)/Apps/rmgc/data/pres2020.rmtree'" | tee { save --append $"($env.AppData)/nushell/vendor/autoload/rmgc-config.nu" }
 
-echo $"\n $env.rmgc_sql = '($env.USERPROFILE)/Apps/rmgc/sql/'" | tee { save --append $"($env.AppData)/nushell/vendor/autoload/rmgc-config.nu" }
+echo $"\n $env.rmgc_sql = '($nu.home-path)/Apps/rmgc/sql/'" | tee { save --append $"($env.AppData)/nushell/vendor/autoload/rmgc-config.nu" }
 
-echo $"\n alias syncdb = cp ($env.USERPROFILE)/Apps/rmgc/data/originaldb/pres2020.rmtree ($env.USERPROFILE)/Apps/rmgc/data/pres2020.rmtree" | tee { save --append $"($env.AppData)/nushell/vendor/autoload/rmgc-config.nu" }
+echo $"\n alias syncdb = cp ($nu.home-path)/Apps/rmgc/data/originaldb/pres2020.rmtree ($nu.home-path)/Apps/rmgc/data/pres2020.rmtree" | tee { save --append $"($env.AppData)/nushell/vendor/autoload/rmgc-config.nu" }
 
-echo $"\n source ($env.USERPROFILE)/Apps/rmgc/src/source-commands.nu" | tee { save --append $"($env.AppData)/nushell/vendor/autoload/rmgc-config.nu" }
+echo $"\n source ($nu.home-path)/Apps/rmgc/src/source-commands.nu" | tee { save --append $"($env.AppData)/nushell/vendor/autoload/rmgc-config.nu" }
 
 print "Configuration complete.  These are the configuration settings:"
 open $"($env.AppData)/nushell/vendor/autoload/rmgc-config.nu"
 
 # Create a copy of demo RM database so syncdb works end-to-end
-mkdir $"($env.USERPROFILE)/Apps/rmgc/data/originaldb"
-cp $"($env.USERPROFILE)/Apps/rmgc/data/pres2020.rmtree" $"($env.USERPROFILE)/Apps/rmgc/data/originaldb/pres2020.rmtree"
+mkdir $"($nu.home-path)/Apps/rmgc/data/originaldb"
+cp $"($nu.home-path)/Apps/rmgc/data/pres2020.rmtree" $"($nu.home-path)/Apps/rmgc/data/originaldb/pres2020.rmtree"
 
 print
 print $"(ansi green_bold)Installation Complete!(ansi reset)"  
